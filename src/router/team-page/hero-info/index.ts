68d8f0efcfa1bd2a5ffe381d5d59ef632236b5c4
dioxs <diomonix@yandex.ua>
2017-09-16 12:47:27 +0300

    fix 2

--- src/router/team-page/hero-info/index.ts
+++ src/router/team-page/hero-info/index.ts
@@ -1,17 +1,13 @@
-import { observer, inject} from 'mobx-react'
+import { observer, inject } from 'mobx-react'
 import { withRouter } from 'react-router'
-import { compose, mapProps, branch, renderNothing  } from 'recompose'
+import { compose, mapProps } from 'recompose'
 
 import Component from './component'
 
 export default compose(
   inject('store'),
   withRouter,
-  observer,
-  branch(
-    store => !store.localStore,
-    renderNothing
-  ),
+  observer,=
   mapProps(
     ({ store }) => {
       const id = store.localStore.selectedHero
